a3d5275c55cb8fb314734cfc47a933a5210b8a38
batuhan içöz <batuhan@icoz.co>
2026-02-12 01:47:39 +0100

    Improve build reliability and add CI build job

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-BINARY_NAME=mautrix-teams go tool maubuild "$@"
+set -eu
+
+if go tool maubuild -h >/dev/null 2>&1; then
+    BINARY_NAME=mautrix-teams go tool maubuild "$@"
+else
+    echo "maubuild not available, falling back to go build" >&2
+    go build -o mautrix-teams ./cmd/mautrix-teams
+fi
